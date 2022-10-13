8c3f62f96307b132f5607969a0587bf847f4418a
Tekyto <Tekyto2014@gmail.com>
2022-10-13 19:54:04 +0300

    num1+num3; num2-num3 ; a*6 ; b*2; c+d

--- index.cpp
+++ index.cpp
@@ -2,5 +2,10 @@
 #include <string>
 
 int operations(int num1, int num2, int num3) {
-	return 0;
+	int a = num1 + num3;
+	int b = num2 - num3;
+	int c = a * 6;
+	int d = b * 2;
+	int e = c + d;
+	return e;
 }

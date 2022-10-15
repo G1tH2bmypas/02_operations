261e6c7ea4d784b084b5e18b664e25b3005f613d
Tekyto <Tekyto2014@gmail.com>
2022-10-15 12:24:39 +0300

    One line

--- index.cpp
+++ index.cpp
@@ -2,10 +2,5 @@
 #include <string>
 
 int operations(int num1, int num2, int num3) {
-	int a = num1 + num3;
-	int b = num2 - num3;
-	int c = a * 6;
-	int d = b * 2;
-	int e = c + d;
-	return e;
+	return ((num1 + num3) * 6 + ( num2 - num3 ) * 2);
 }
